c15d252937ebc2145ebb6516155bd559a9c9d99e
RJKZ <rjkz808@list.ru>
2018-08-01 13:15:27 +0300

    update TestCyberCoin.sol (Nick)

--- test/TestCyberCoin.sol
+++ test/TestCyberCoin.sol
@@ -1,5 +1,49 @@
 pragma solidity ^0.4.24;
 
-contract TestCyber {
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/CyberCoin.sol";
+
+contract TestCyberCoin {
+
+  function testERC721ReceivedDeployed() public {
+    CyberCoin cyber = CyberCoin(DeployedAddresses.CyberCoin());
+    bytes4 expected = 0x40253ed2;
+    Assert.equal(
+      cyber.ERC721_RECEIVED(),
+      expected,
+      "ERC721_RECEIVED magic value should be equal to 0xf4945cc6"
+    );
+  }
+
+  function testERC721ReceivedNew() public {
+    CyberCoin cyber = new CyberCoin();
+    bytes4 expected = 0x40253ed2;
+    Assert.equal(
+      cyber.ERC721_RECEIVED(),
+      expected,
+      "ERC721_RECEIVED magic value should be equal to 0xf4945cc6"
+    );
+  }
+
+  function testInitialTotalSupplyDeployed() public {
+    CyberCoin cyber = CyberCoin(DeployedAddresses.CyberCoin());
+    uint expected = 0;
+    Assert.equal(
+      cyber.totalSupply(), 
+      expected, 
+      "Initial tokens amount should be equal to 0"
+    );
+  }
+
+  function testInitialTotalSupplyNew() public {
+    CyberCoin cyber = new CyberCoin();
+    uint expected = 0;
+    Assert.equal(
+      cyber.totalSupply(), 
+      expected, 
+      "Initial tokens amount should be equal to 0"
+    );
+  }
 	
 }
