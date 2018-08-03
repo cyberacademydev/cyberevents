f7f49b8cf74aab02b3a9dd469279412ea8de8cc5
RJKZ <rjkz808@list.ru>
2018-08-03 18:07:19 +0300

    fix migrations (Nick)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const CyberCoin = artifacts.require("./CyberCoin.sol");
+const CyberCore = artifacts.require("./CyberCore.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(CyberCoin);
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(CyberCoin).then(function() {
+    return deployer.deploy(CyberCore, CyberCoin.address)
+  });
 };
