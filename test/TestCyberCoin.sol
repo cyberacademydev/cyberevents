6e0bd6f95dc300829467bfa51d9ce0bd1bc77fea
RJKZ <rjkz808@list.ru>
2018-08-03 19:33:09 +0300

    update solidity CyberCoin test (Nick)

--- test/TestCyberCoin.sol
+++ test/TestCyberCoin.sol
@@ -14,5 +14,16 @@ contract TestCyberCoin {
       "Initial tokens amount should be equal to 0"
     );
   }
+
+  function testMinterAddress() public {
+    CyberCoin cyber = new CyberCoin();
+    address expected = msg.sender;
+    cyber.setMinter(expected);
+    Assert.equal(
+      cyber.minter(),
+      expected,
+      "Unexpected minter address"
+    );
+  }
 	
 }
