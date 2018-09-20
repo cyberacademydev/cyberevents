9bae5fd46119362b8c6f61aa69a7a7f4ace9175c
rjkz808 <38258624+rjkz808@users.noreply.github.com>
2018-09-20 19:51:45 +0300

    Add networks and user mnemonic reader

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,44 @@
+const readline = require('readline-sync');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = readline.question('Enter your 12 word mnemonic\n');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    live: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/'),
+      network_id: '1'
+    },
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*'
+    },
+    main: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/'),
+      network_id: '1'
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/'),
+      network_id: '3'
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/'),
+      network_id: '4'
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://kovan.infura.io/'),
+      network_id: '42'
+    },
+    ganache: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*'
+    },
+    ganachecli: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
+    }
+  }
 };
